0f0f1c7915a36fdbf9ef33fc61dec17bedcf5377
Roberto Racz <robertorac94@hotmail.com>
2022-08-08 15:55:08 +0200

    getting parameters work

--- src/main/java/com/codecool/shop/controller/CheckOutController.java
+++ src/main/java/com/codecool/shop/controller/CheckOutController.java
@@ -16,15 +16,24 @@ public class CheckOutController extends HttpServlet {
     @Override
     public void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
+        String name = req.getParameter("name");
+        String email = req.getParameter("email");
+        int phoneNumber = Integer.parseInt(req.getParameter("phone-number"));
+        String billingAddress = req.getParameter("billing-address");
+        String shippingAddress = req.getParameter("shipping-address");
 
+        /*make interface checkOut Interface or record
+        create implementation for the checkout OR
+        create new checkOutInfo object with the parameters
+        give it over to the verification Service
+        and BOOM
+         */
 
     }
 
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-
-        System.out.println("erbvceashsdc");
 
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
